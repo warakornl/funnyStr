3a957981cb81f2d52c91d444bb37ec380995dd93
warakornl <l_warakorn@outlook.com>
2020-05-01 20:38:21 +0700

    Edited name and Added unit test api

--- src/test/java/com/devChallenge/funnyStr/FunnyStrApplicationTests.java
+++ src/test/java/com/devChallenge/funnyStr/FunnyStrApplicationTests.java
@@ -3,7 +3,16 @@ package com.devChallenge.funnyStr;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.devChallenge.services.ChallengeServices;
 
 @SpringBootTest
 class FunnyStrApplicationTests {
@@ -46,6 +55,21 @@ class FunnyStrApplicationTests {
 	public void wordShouldBe_avaj1evol1i_InBase64() {
 		ChallengeServices serv = new ChallengeServices("aSBsb3ZlIGphdmE=");
 		assertEquals("YXZhajFldm9sMWk=", serv.encode());
+	}
+	
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Test
+	public void updateProduct() throws Exception {
+		MockMvc mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		String uri = "/devchallenge";
+		String funnyStr = "aSBsb3ZlIGphdmE=";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri).param("funnyStr",funnyStr)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		assertEquals(content, "YXZhajFldm9sMWk=");
 	}
 	 
 }
